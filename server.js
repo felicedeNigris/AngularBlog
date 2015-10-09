65a7f0d4ec5efc0972ebebe1a1969d16d09d9dce
Felice DeNigris <felideni@gmail.com>
2015-10-09 15:08:35 -0500

    trying to fix dependency error

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-app.use(express.static(__dirname)); //send angular files
-app.use(express.static(__dirname + '/public')); // send static files css
-app.get('/',function(req,res){
-  res.sendfile('public/views/index.html'); //send html
+
+app.set('port',(process.env.PORT || 7000));
+
+app.use(express.static(__dirname)); // add js 
+app.use(express.static(path.join(__dirname, 'public'))); // add css 
+
+//routes will go here
+app.get('/', function(req,res){
+  res.sendFile(path.resolve('public/views/index.html'));
 });
 
-app.set('port', (process.env.PORT || 7000));
-
-
-var server = app.listen(app.get('port'));
+var server = app.listen(process.env.PORT || 7000, function(){
+  console.log('Express App started');
+});
